f81f531519f51494bc6d074b2d800d1bae52072e
infinite8loop <musalannagarimanoj1999@gmail.com>
2022-04-18 15:10:38 +0530

    /~

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # One time setup for the command to work from any directory by any user.
-#cd /
+#cd ~/
 #git clone https://gitlab.com/MAKO_ManojKumar/cpu-mem-macos.git
 #cd cpu-mem-macos
-echo 'export PATH="/cpu-mem:$PATH"' >> ~/.zshrc
+echo 'export PATH="~/cpu-mem:$PATH"' >> ~/.zshrc
 echo "alias brain='bash cpu-mem.sh'" >>  ~/.zshrc
 sleep 2
 source ~/.zshrc
