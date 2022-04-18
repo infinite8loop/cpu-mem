1d7fd5e79586b792e72111ef50eb2c5556abbee0
infinite8loop <musalannagarimanoj1999@gmail.com>
2022-04-18 14:53:05 +0530

    path updated

--- cpu-mem.sh
+++ cpu-mem.sh
@@ -1,4 +1,4 @@
-cd ~/cpu-mem-macos
+cd ~/cpu-mem
 touch data.txt
 top -l 1 | grep -E "^CPU|^Phys" > data.txt
 cat data.txt |xargs -I {} osascript -e 'display notification "{}" with title "CPU & Memory"' && cat data.txt |xargs -I {} osascript -e 'say "{}"'
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #cd ~/
 #git clone https://gitlab.com/MAKO_ManojKumar/cpu-mem-macos.git
 #cd cpu-mem-macos
-echo 'export PATH="~/cpu-mem-macos:$PATH"' >> ~/.zshrc
+echo 'export PATH="~/cpu-mem:$PATH"' >> ~/.zshrc
 echo "alias brain='bash cpu-mem.sh'" >>  ~/.zshrc
 sleep 2
 source ~/.zshrc
